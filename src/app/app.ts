c407976177fb201b116d391f1c094452d7593fce
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2026-03-13 00:00:58 -0300

    feat: component secondaryButton

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
 import { CommonModule } from '@angular/common';
 import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
