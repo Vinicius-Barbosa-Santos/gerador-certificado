d1c5b567a06d65e234231c044168ba09a2595e85
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2026-03-30 19:19:47 -0300

    feat: add routerLink

--- src/app/_components/item-certificado/item-certificado.ts
+++ src/app/_components/item-certificado/item-certificado.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../secondary-button/secondary-button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [CommonModule, SecondaryButton],
+  imports: [CommonModule, SecondaryButton, RouterLink],
   templateUrl: './item-certificado.html',
   styleUrl: './item-certificado.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Certificado } from './pages/certificado/certificado';
+import { CertificadoForm } from './pages/certificado-form/certificado-form';
+import { Certificados } from './pages/certificados/certificados';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Certificados,
+  },
+  {
+    path: 'certificados/novo',
+    component: CertificadoForm,
+  },
+  {
+    path: 'certificados/:id',
+    component: Certificado,
+  },
+];
