a96444985be18724356f41eea44038e1378fd457
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2026-04-06 16:30:17 -0300

    feat(certificado): add download functionality for certificate
    Add html2canvas dependency to enable converting the certificate HTML to an image.
    Implement downloadCertificado method that captures the certificate container and triggers a PNG download.
    Add click handler to the download button and reference the certificate container element.

--- src/app/pages/certificado/certificado.ts
+++ src/app/pages/certificado/certificado.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CertificadoService } from '../../_services/certificado.service';
 import { Certificado as CertificadoModel } from '../../interfaces/certificado';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-certificado',
@@ -14,6 +15,8 @@ export class Certificado implements OnInit {
   id: string | null = null;
   certificado: CertificadoModel | undefined;
 
+  @ViewChild('certificadoContainer') certificadoElement!: ElementRef;
+
   constructor(
     private CertificadoService: CertificadoService,
     private route: ActivatedRoute,
@@ -23,6 +26,18 @@ export class Certificado implements OnInit {
     this.route.paramMap.subscribe((params) => {
       this.id = params.get('id');
       this.certificado = this.CertificadoService.certificados.find((item) => item.id == this.id);
+    });
+  }
+
+  downloadCertificado() {
+    if (this.certificado == undefined) {
+      return;
+    }
+    html2canvas(this.certificadoElement.nativeElement, { scale: 2 }).then((canvas) => {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png');
+      link.download = 'certificado_' + this.certificado?.nome.replaceAll(' ', '_') + '.png';
+      link.click();
     });
   }
 }
